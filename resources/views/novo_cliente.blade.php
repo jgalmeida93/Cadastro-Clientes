c6dafd84f52f66496f7df84a168dd57c4b6e5d3a
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-01-27 10:45:37 -0300

    :sparkles: Implementa validação campo a campo

--- resources/views/novo_cliente.blade.php
+++ resources/views/novo_cliente.blade.php
@@ -27,19 +27,47 @@
                             @csrf
                             <div class="form-group">
                                 <label for="nome">Nome</label>
-                                <input type="text" id="nome" class="form-control" name="nome" placeholder="Nome do cliente">
+                            <input type="text" id="nome" class="form-control 
+                            {{ $errors->has('nome') ? 'is-invalid' : '' }}" name="nome" placeholder="Nome do cliente">
+                                @if ($errors->has('nome'))
+                                    <div class="invalid-feedback">
+                                        {{ $errors->first('nome') }}
+                                    </div>
+                                @endif
                             </div>
                             <div class="form-group">
                                 <label for="idade">Idade</label>
-                                <input type="numer" id="idade" class="form-control" name="idade" placeholder="Idade do cliente">
+                                <input type="numer" id="idade" 
+                            class="form-control {{ $errors->has('idade') ? 'is-invalid' : '' }}" 
+                                name="idade" 
+                                placeholder="Idade do cliente">
+                            @if ($errors->has('idade'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('idade') }}
+                                </div>
+                            @endif
                             </div>
                             <div class="form-group">
                                 <label for="enderec">Endereço</label>
-                                <input type="text" id="endereco" class="form-control" name="endereco" placeholder="Endereço do cliente">
+                                <input type="text" id="endereco" 
+                            class="form-control {{ $errors->has('endereco') ? 'is-invalid' : '' }}" 
+                                name="endereco" placeholder="Endereço do cliente">
+                            @if ($errors->has('endereco'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('endereco') }}
+                                </div>
+                            @endif
                             </div>
                             <div class="form-group">
                                 <label for="email">Email</label>
-                                <input type="text" id="email" class="form-control" name="email" placeholder="Email do cliente">
+                                <input type="text" id="email" 
+                            class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" 
+                                name="email" placeholder="Email do cliente">
+                            @if ($errors->has('email'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('email') }}
+                                </div>
+                            @endif
                             </div>
 
                             <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
@@ -47,15 +75,7 @@
 
                         </form>
                     </div>
-                    @if ($errors->any())
-                        <div class="card-footer">
-                            @foreach ($errors->all() as $err)
-                                <div class="alert alert-danger" role="alert">
-                                    {{ $err }}
-                                </div>
-                            @endforeach
-                        </div>
-                    @endif
+
                 </div>
             </div>
         </div>
